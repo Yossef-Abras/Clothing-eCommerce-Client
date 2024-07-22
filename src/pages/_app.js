722df907ea7121e704437fd6c923bab0c8a77a26
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-07-22 13:37:36 +0300

    fix app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,6 @@
-// import { NextUIProvider } from "@nextui-org/system";
-import Providers from "@/components/Providers";
 import HomeLayout from "@/layouts/HomeLayout";
 import "@/styles/globals.css";
-<<<<<<< HEAD
-
-=======
->>>>>>> 0b3519b58f7f70efe66ca04a390791b10fbc6c20
+import { NextUIProvider } from "@nextui-org/system";
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
