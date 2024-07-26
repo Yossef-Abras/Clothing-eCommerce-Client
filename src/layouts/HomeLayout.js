6e89171232faecf2086548eecc1a4784ce3030a4
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-07-26 16:20:42 +0300

    fix app

--- src/layouts/HomeLayout.js
+++ src/layouts/HomeLayout.js
@@ -1,12 +1,10 @@
-import MyNavbar from "@/components/MyNavbar";
-import Footer from "@/components/Footer";
+import MyNavbar from "../components/MyNavbar";
+import Footer from "../components/Footer";
 export default function HomeLayout({ children }) {
   return (
     <div>
       <MyNavbar />
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
       <Footer />
     </div>
   );
