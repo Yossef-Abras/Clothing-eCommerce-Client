4bb0bb0b5b76603f0cea4fb73ab84530b4e42d1c
yossef_abras <yossefabras@gmail.com>
2024-07-27 11:56:53 +0300

    add moving circles

--- src/components/CardHome.js
+++ src/components/CardHome.js
@@ -1,55 +1,28 @@
-import { Progress } from '@nextui-org/react';
-import { useRef } from 'react';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Button, Image } from '@nextui-org/react';
 
-export default function CardHome() {
-    const scrollRef = useRef(null);
-    const scroll = (direction) => {
-        if (scrollRef.current) {
-            scrollRef.current.scrollBy({
-                left: direction === 'left' ? -200 : 200,
-                behavior: 'smooth'
-            });
-        }
-    };
-
+export default function CardHome({ prodectname, price, img }) {
     return (
-        <div className="relative mx-auto m-5">
-            <Progress size='sm'
-                color="warning"
-                label="More Buy"
-                value={100}
-                className='font-bold text-orange-400'
-            />
-            <div
-                className="overflow-x-auto whitespace-nowrap scroll-smooth"
-                ref={scrollRef}
-            >
-                <div className="inline-block w-60 h-40 bg-gray-200 p-4 m-2 rounded-lg shadow-lg">
-                    <h3 className="text-lg font-bold">Card 1</h3>
-                    <p>Content for card 1</p>
-                </div>
-                <div className="inline-block w-60 h-40 bg-gray-200 p-4 m-2 rounded-lg shadow-lg">
-                    <h3 className="text-lg font-bold">Card 2</h3>
-                    <p>Content for card 2</p>
-                </div>
-                <div className="inline-block w-60 h-40 bg-gray-200 p-4 m-2 rounded-lg shadow-lg">
-                    <h3 className="text-lg font-bold">Card 3</h3>
-                    <p>Content for card 3</p>
+        <div className="min-w-60 m-2 rounded-lg shadow-lg border-1 border-orange-400 bg-white">
+            <div>
+                <Image
+                    width={4000}
+                    height={3000}
+                    src={img}
+                    alt="Description of the image"
+                    className='w-fit'
+
+                />
+            </div>
+            <hr></hr>
+            <div className='flex flex-col items-center gap-2  text-orange-400 px-2 pb-4 rounded-lg'>
+                <p className="text-lg font-bold  ">{prodectname}</p>
+                <p>{price}</p>
+                <div className='' >
+                    <Button className="border-2 rounded-xl border-orange-400 bg-inherit hover:bg-orange-400">
+                        Add to cart
+                    </Button>
                 </div>
             </div>
-            <button
-                onClick={() => scroll('left')}
-                className="absolute left-0 top-1/2 transform -translate-y-1/2 p-2 bg-orange-300 text-white rounded-full shadow-lg"
-            >
-                <FaChevronLeft />
-            </button>
-            <button
-                onClick={() => scroll('right')}
-                className="absolute right-0 top-1/2 transform -translate-y-1/2 p-2 bg-orange-300 text-white rounded-full shadow-lg"
-            >
-                <FaChevronRight />
-            </button>
-        </div >
+        </div>
     );
 };
