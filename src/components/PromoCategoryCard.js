7eb6df965d0ac134758979e703162ed76360c9aa
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-07-29 20:13:25 +0300

    fixing responsive

--- src/components/PromoCategoryCard.js
+++ src/components/PromoCategoryCard.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function PromoCategoryCard({ type, categories, message }) {
   return (
-    <Card className="shadow-lg m-4 md:m-10 md:max-h-[300px]">
+    <Card className="shadow-lg m-4 md:m-10 md:max-h-[275px] max-h-[475px] md:h-[275px] h-[475px]">
       <div className="flex flex-col pb-3">
         <div className="flex flex-col md:flex-row w-full">
           <div className="flex bg-orange-300 rounded-b-full md:w-20 w-full h-14 md:h-20 justify-center mx-auto md:mx-4 font-bold">
@@ -12,12 +12,12 @@ export default function PromoCategoryCard({ type, categories, message }) {
           </div>
           <div className="flex flex-col-reverse md:flex-row w-full md:items-center">
             {/* Link & Message */}
-            <div className="w-full self-center px-0 md:w-1/2">
+            <div className="w-full md:h-full self-center px-0 md:w-1/2">
               <p className="hidden md:block">
                 <span className="font-bold text-orange-400">Saramoda </span>
                 {message}
               </p>
-              <div className="flex justify-start flex-wrap md:px-0 px-4 gap-1 mt-4">
+              <div className="flex justify-center items-center flex-wrap md:px-0 px-4 gap-1 mt-4 h-[100px]">
                 {categories.map((catigory) => (
                   <Link
                     key={catigory}
@@ -30,20 +30,18 @@ export default function PromoCategoryCard({ type, categories, message }) {
               </div>
             </div>
             {/* Image */}
-            <div className="flex w-full h-full md:w-1/2 mt-4 md:mt-0">
+            <div className="flex justify-center w-full md:w-1/2 mt-4 md:mt-0">
               <Image
                 width={4000}
                 height={3000}
                 src="/img/ss.jpg"
                 alt="Description of the image"
-                className="w-full h-full rounded-lg"
+                className="w-full md:h-full h-[275px] rounded-lg"
               />
             </div>
           </div>
         </div>
-
       </div>
     </Card>
-
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,201 +7,201 @@ import CardHome from "../components/CardHome";
 import { Button, Progress } from "@nextui-org/react";
 import MovingCircles from "../components/MovingCircles";
 
-
 function SampleNextArrow(props) {
-	const { className, style, onClick } = props;
-	return (
-		<div
-			className={className}
-			style={{
-				...style,
-				display: "block",
-				background: "#fb923c",
-				borderRadius: "50%",
-				paddingTop: "1px",
-			}}
-			onClick={onClick}
-		/>
-	);
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fb923c",
+        borderRadius: "50%",
+        paddingTop: "1px",
+      }}
+      onClick={onClick}
+    />
+  );
 }
 
 function SamplePrevArrow(props) {
-	const { className, style, onClick } = props;
-	return (
-		<div
-			className={className}
-			style={{
-				...style,
-				display: "block",
-				background: "#fb923c",
-				borderRadius: "50%",
-				paddingTop: "1px",
-			}}
-			onClick={onClick}
-		/>
-	);
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fb923c",
+        borderRadius: "50%",
+        paddingTop: "1px",
+      }}
+      onClick={onClick}
+    />
+  );
 }
 
 export default function Home() {
-	const [promoData, setPromoData] = useState([
-		{
-			type: "Women",
-			message:
-				"offers elegance and style with our exclusive women's fashion collection.",
-			categories: [
-				"short",
-				"tees",
-				"short",
-				"tees",
-				"short",
-				"tees",
-				"short",
-				"tees",
-			],
-		},
-		{
-			type: "Men",
-			message:
-				"brings sophistication and flair with our exclusive men's fashion collection.",
-			categories: ["tees"],
-		},
-	]);
-	const settings = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		arrows: false,
-	};
-	const settings1 = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		nextArrow: <SamplePrevArrow />,
-		prevArrow: <SampleNextArrow />,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 640,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	};
-	const products = [
-		{
-			id: 1,
-			name: 'Product 1',
-			price: '$10.00',
-			img: "/img/ss.jpg"
-		},
-		{
-			id: 2,
-			name: 'Product 2',
-			price: '$20.00',
-			img: "/img/ss.jpg"
-		},
-		{
-			id: 3,
-			name: 'Product 3',
-			price: '$20.00',
-			img: "/img/ss.jpg"
-		},
-		{
-			id: 4,
-			name: 'Product 4',
-			price: '$20.00',
-			img: "/img/ss.jpg"
-		},
-		{
-			id: 5,
-			name: 'Product 5',
-			price: '$20.00',
-			img: "/img/ss.jpg"
-		},
-		{
-			id: 6,
-			name: 'Product 5',
-			price: '$20.00',
-			img: "/img/ss.jpg"
-		},
-	];
-	return (
-		<main className="relative flex min-h-screen flex-col items-center justify-start">
-			<MovingCircles numCircles={15} />
-			<div className="w-full flex justify-center bg-gradient-to-r from-orange-100/50 via-orange-200/50 to-orange-300/50 backdrop-blur-md">
-				<Slider className="md:w-[700px] w-[300px]" {...settings}>
-					{promoData.map((value) => (
-						<PromoCategoryCard
-							key={value.type}
-							type={value.type}
-							message={value.message}
-							categories={value.categories}
-						/>
-					))}
-				</Slider>
+  const [promoData, setPromoData] = useState([
+    {
+      type: "Women",
+      message:
+        "offers elegance and style with our exclusive women's fashion collection.",
+      categories: [
+        "short",
+        "tees",
+        "short",
+        "tees",
+        "short",
+        "tees",
+        "short",
+        "tees",
+      ],
+    },
+    {
+      type: "Men",
+      message:
+        "brings sophistication and flair with our exclusive men's fashion collection.",
+      categories: ["tees"],
+    },
+  ]);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+  };
+  const settings1 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SamplePrevArrow />,
+    prevArrow: <SampleNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: "$10.00",
+      img: "/img/ss.jpg",
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: "$20.00",
+      img: "/img/ss.jpg",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: "$20.00",
+      img: "/img/ss.jpg",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: "$20.00",
+      img: "/img/ss.jpg",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      price: "$20.00",
+      img: "/img/ss.jpg",
+    },
+    {
+      id: 6,
+      name: "Product 5",
+      price: "$20.00",
+      img: "/img/ss.jpg",
+    },
+  ];
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-start">
+      <MovingCircles numCircles={15} />
+      <div className="w-full flex justify-center bg-gradient-to-r from-orange-100/50 via-orange-200/50 to-orange-300/50 backdrop-blur-md">
+        <Slider className="md:w-[700px] w-[350px]" {...settings}>
+          {promoData.map((value) => (
+            <PromoCategoryCard
+              key={value.type}
+              type={value.type}
+              message={value.message}
+              categories={value.categories}
+            />
+          ))}
+        </Slider>
+      </div>
+      <div></div>
+      <div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
+        <label className="text-orange-400 text-lg font-bold">Top Sellers</label>
+        <Progress
+          size="sm"
+          color="warning"
+          value={100}
+          className="font-bold text-orange-400"
+        />
 
-			</div>
-			<div>
-			</div>
-			<div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
-				<label className="text-orange-400 text-lg font-bold">Top Sellers</label>
-				<Progress size='sm'
-					color="warning"
-					value={100}
-					className='font-bold text-orange-400'
-				/>
+        <div className="relative w-[86%] mx-auto m-3">
+          <div className="flex">
+            <Slider className="w-full" {...settings1}>
+              {products.map((product) => (
+                <CardHome
+                  key={product.id}
+                  prodectname={product.name}
+                  price={product.price}
+                  img={product.img}
+                />
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+      <div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
+        <label className="text-orange-400 text-lg font-bold">
+          Special Offers
+        </label>
+        <Progress
+          size="sm"
+          color="warning"
+          value={100}
+          className="font-bold text-orange-400"
+        />
 
-				<div className="relative w-[86%] mx-auto m-3">
-					<div className="flex">
-						<Slider className="w-full" {...settings1}>
-							{products.map((product) => (
-								<CardHome
-									key={product.id}
-									prodectname={product.name}
-									price={product.price}
-									img={product.img}
-								/>
-							))}
-						</Slider>
-					</div>
-				</div >
-			</div>
-			<div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
-				<label className="text-orange-400 text-lg font-bold">Special Offers</label>
-				<Progress size='sm'
-					color="warning"
-					value={100}
-					className='font-bold text-orange-400'
-				/>
-
-				<div className="relative w-[86%] mx-auto m-3">
-					<div className="flex">
-						<Slider className="w-full" {...settings1}>
-							{products.map((product) => (
-								<CardHome
-									key={product.id}
-									prodectname={product.name}
-									price={product.price}
-									img={product.img}
-								/>
-							))}
-						</Slider>
-					</div>
-				</div >
-			</div>
-
-		</main>
-	);
+        <div className="relative w-[86%] mx-auto m-3">
+          <div className="flex">
+            <Slider className="w-full" {...settings1}>
+              {products.map((product) => (
+                <CardHome
+                  key={product.id}
+                  prodectname={product.name}
+                  price={product.price}
+                  img={product.img}
+                />
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
