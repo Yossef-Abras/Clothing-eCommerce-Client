8d07102f2c88c3389720b9ca5538eae8299bd711
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-08-12 19:41:20 +0300

    fixing

--- src/components/CardHome.js
+++ src/components/CardHome.js
@@ -1,39 +1,37 @@
-import { Button, Image } from '@nextui-org/react';
-import { useState, useEffect } from 'react';
-import { MdFavorite, MdFavoriteBorder } from 'react-icons/md';
+import { Button, Image } from "@nextui-org/react";
+import { MdFavorite, MdFavoriteBorder } from "react-icons/md";
 
-export default function CardHome({ productId, prodectname, price, img, isFavorite }) {
-    // const [isfavorite, setIsFavorite] = useState(false)
-    // const favorites = () => {
-    //     setIsFavorite(productId, !isfavorite)
-    // }
-    return (
-        <div className="min-w-60 m-2 rounded-lg shadow-lg border-1 border-orange-400 bg-white">
-            <div className='flex justify-center'>
-                <Image
-                    width={4000}
-                    height={3000}
-                    src={img}
-                    alt={prodectname}
-                    className='w-fit'
-                />
-            </div>
-            <hr />
-            <div className='flex justify-between px-2 pb-4 rounded-lg'>
-                <div className='flex flex-col'>
-                    <p className="text-lg font-bold">{prodectname}</p>
-                    <p>{price}</p>
-                </div>
-                <Button
-                    className='bg-inherit text-lg min-w-0 w-8 p-0'
-                    style={{ cursor: 'pointer' }}
-
-                >
-                    {isFavorite ? <MdFavorite style={{ color: 'red' }} /> : <MdFavoriteBorder />}
-                </Button>
-            </div>
+export default function CardHome({ prodectname, price, img, isFavorite }) {
+  return (
+    <div className="min-w-60 m-2 rounded-lg shadow-lg border-1 border-orange-400 bg-white">
+      <div className="flex justify-center">
+        <Image
+          width={4000}
+          height={3000}
+          src={img}
+          alt={prodectname}
+          className="w-fit"
+        />
+      </div>
+      <hr />
+      <div className="flex justify-between px-2 pb-4 rounded-lg">
+        <div className="flex flex-col">
+          <p className="text-lg font-bold">{prodectname}</p>
+          <p>{price}</p>
         </div>
-    );
+        <Button
+          className="bg-inherit text-lg min-w-0 w-8 p-0"
+          style={{ cursor: "pointer" }}
+        >
+          {isFavorite ? (
+            <MdFavorite style={{ color: "red" }} />
+          ) : (
+            <MdFavoriteBorder />
+          )}
+        </Button>
+      </div>
+    </div>
+  );
 }
 // const { addToFavorites, removeFromFavorites, isFavorite } = useFavorites();
 // useEffect(() => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,188 +6,192 @@ import "slick-carousel/slick/slick-theme.css";
 import CardHome from "../components/CardHome";
 import { Button, Progress, Spinner } from "@nextui-org/react";
 import MovingCircles from "../components/MovingCircles";
-import { getCategories, getProducts, getSubCategories } from "../../global/product";
+import {
+  getCategories,
+  getProducts,
+  getSubCategories,
+} from "../../global/product";
 
 function SampleNextArrow(props) {
-	const { className, style, onClick } = props;
-	return (
-		<div
-			className={className}
-			style={{
-				...style,
-				display: "block",
-				background: "#fb923c",
-				borderRadius: "50%",
-				paddingTop: "1px",
-			}}
-			onClick={onClick}
-		/>
-	);
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fb923c",
+        borderRadius: "50%",
+        paddingTop: "1px",
+      }}
+      onClick={onClick}
+    />
+  );
 }
 
 function SamplePrevArrow(props) {
-	const { className, style, onClick } = props;
-	return (
-		<div
-			className={className}
-			style={{
-				...style,
-				display: "block",
-				background: "#fb923c",
-				borderRadius: "50%",
-				paddingTop: "1px",
-			}}
-			onClick={onClick}
-		/>
-	);
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: "block",
+        background: "#fb923c",
+        borderRadius: "50%",
+        paddingTop: "1px",
+      }}
+      onClick={onClick}
+    />
+  );
 }
 
 export default function Home() {
+  const settings1 = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <SamplePrevArrow />,
+    prevArrow: <SampleNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
 
-	const settings1 = {
-		dots: true,
-		infinite: true,
-		speed: 500,
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		nextArrow: <SamplePrevArrow />,
-		prevArrow: <SampleNextArrow />,
-		responsive: [
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 2,
-				},
-			},
-			{
-				breakpoint: 640,
-				settings: {
-					slidesToShow: 1,
-				},
-			},
-		],
-	};
+  const [TopSellersProductsproducts, setTopSellersProducts] = useState([]);
+  const [TopOffersproducts, setTopOffersProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [lodaing, setLoading] = useState(true);
+  const getTopSellersProducts = async () => {
+    try {
+      const res = await getProducts();
+      setTopSellersProducts(res.data);
+    } catch (error) {
+      console.error("Error fetching Top Sellers Products:", error);
+      throw error;
+    }
+  };
 
-	const [TopSellersProductsproducts, setTopSellersProducts] = useState([]);
-	const [TopOffersproducts, setTopOffersProducts] = useState([]);
-	const [categories, setCategories] = useState([]);
-	const [lodaing, setLoading] = useState(true);
-	const getTopSellersProducts = async () => {
-		try {
-			const res = await getProducts();
-			setTopSellersProducts(res.data);
-		} catch (error) {
-			console.error("Error fetching Top Sellers Products:", error);
-			throw error;
-		}
-	};
+  const getTopOffersProducts = async () => {
+    try {
+      const res = await getProducts();
+      setTopOffersProducts(res.data);
+    } catch (error) {
+      console.error("Error fetching Top Offers Products:", error);
+      throw error;
+    }
+  };
 
-	const getTopOffersProducts = async () => {
-		try {
-			const res = await getProducts();
-			setTopOffersProducts(res.data);
-		} catch (error) {
-			console.error("Error fetching Top Offers Products:", error);
-			throw error;
-		}
-	};
+  const getAllCategories = async () => {
+    try {
+      const res = await getSubCategories();
+      setCategories(res.data);
+    } catch (error) {
+      console.error("Error fetching Categories:", error);
+      throw error;
+    }
+  };
 
-	const getAllCategories = async () => {
-		try {
-			const res = await getSubCategories();
-			setCategories(res.data);
-		} catch (error) {
-			console.error("Error fetching Categories:", error);
-			throw error;
-		}
-	};
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        await Promise.all([
+          getTopSellersProducts(),
+          getTopOffersProducts(),
+          getAllCategories(),
+        ]);
+      } catch (error) {
+        console.error("Error occurred during fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				setLoading(true)
-				await Promise.all([getTopSellersProducts(), getTopOffersProducts(), getAllCategories()]);
-			} catch (error) {
-				console.error("Error occurred during fetching data:", error);
-			} finally {
-				setLoading(false);
-			}
-		};
+    fetchData();
+  }, []);
+  if (lodaing) {
+    return (
+      <div className="min-h-[400px] text-orange-500 flex justify-center items-center">
+        <Spinner color="primary" />
+      </div>
+    );
+  }
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-start">
+      <MovingCircles numCircles={15} />
+      <div className="w-full flex justify-center bg-gradient-to-r from-orange-100/50 via-orange-200/50 to-orange-300/50 backdrop-blur-md">
+        {/* <Slider className="md:w-[700px] w-[300px]"> */}
+        <div className="md:w-[700px] w-[300px]">
+          <PromoCategoryCard categories={categories} />
+        </div>
+        {/* </Slider> */}
+      </div>
+      <div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
+        <label className="text-orange-400 text-lg font-bold">Top Sellers</label>
+        <Progress
+          size="sm"
+          color="warning"
+          value={100}
+          className="font-bold text-orange-400"
+        />
 
-		fetchData();
-	}, []);
-	if (lodaing) {
-		return <div className="min-h-[400px] text-orange-500 flex justify-center items-center">
-			<Spinner color="primary" />
-		</div>
-	}
-	return (
-		<main className="relative flex min-h-screen flex-col items-center justify-start">
-			<MovingCircles numCircles={15} />
-			<div className="w-full flex justify-center bg-gradient-to-r from-orange-100/50 via-orange-200/50 to-orange-300/50 backdrop-blur-md">
-				{/* <Slider className="md:w-[700px] w-[300px]"> */}
-				<div className="md:w-[700px] w-[300px]">
-					<PromoCategoryCard categories={categories}
-					/>
-				</div>
-				{/* </Slider> */}
-
-
-			</div>
-			<div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
-				<label className="text-orange-400 text-lg font-bold">
-					Top Sellers
-				</label>
-				<Progress
-					size="sm"
-					color="warning"
-					value={100}
-					className="font-bold text-orange-400"
-				/>
-
-				<div className="relative w-[86%] mx-auto m-3">
-					<div className="flex">
-						<Slider className="w-full" {...settings1}>
-							{TopSellersProductsproducts.map((product) => (
-								<CardHome
-									key={product._id}
-									productId={product.id}
-									prodectname={product.title}
-									price={product.price}
-									img={product.imageCover}
-
-								/>
-							))}
-						</Slider>
-					</div>
-				</div >
-			</div>
-			<div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
-				<label className="text-orange-400 text-lg font-bold">Special Offers</label>
-				<Progress size='sm'
-					color="warning"
-					value={100}
-					className='font-bold text-orange-400'
-				/>
-				<div className="relative w-[86%] mx-auto m-3">
-					<div className="flex">
-						<Slider className="w-full" {...settings1}>
-							{TopOffersproducts.map((product) => (
-								<CardHome
-									key={product._id}
-									productId={product._id}
-									prodectname={product.title}
-									price={product.price}
-									img={product.imageCover}
-									isFavorite={product.isFavorite}
-
-								/>
-							))}
-						</Slider>
-					</div>
-				</div >
-			</div>
-
-		</main>
-	);
+        <div className="relative w-[86%] mx-auto m-3">
+          <div className="flex">
+            <Slider className="w-full" {...settings1}>
+              {TopSellersProductsproducts.map((product) => (
+                <CardHome
+                  key={product._id}
+                  productId={product._id}
+                  prodectname={product.title}
+                  price={product.price}
+                  img={product.imageCover}
+                />
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+      <div className="w-full backdrop-blur-md m-5 my-10 px-5 pt-4">
+        <label className="text-orange-400 text-lg font-bold">
+          Special Offers
+        </label>
+        <Progress
+          size="sm"
+          color="warning"
+          value={100}
+          className="font-bold text-orange-400"
+        />
+        <div className="relative w-[86%] mx-auto m-3">
+          <div className="flex">
+            <Slider className="w-full" {...settings1}>
+              {TopOffersproducts.map((product) => (
+                <CardHome
+                  key={product._id}
+                  productId={product._id}
+                  prodectname={product.title}
+                  price={product.price}
+                  img={product.imageCover}
+                  isFavorite={product.isFavorite}
+                />
+              ))}
+            </Slider>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
