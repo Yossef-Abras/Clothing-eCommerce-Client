1cc48bf29c56970191263f934518c501eea5fb61
yossef_abras <yossefabras@gmail.com>
2024-08-13 15:21:23 +0300

    fixing

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -41,19 +41,19 @@ export default function index() {
 	];
 	return (
 		<div>
-			<div className="flex justify-between items-center">
-				<label className="text-orange-400 text-lg font-bold">Top Sellers</label>
-				<Button className=" p-0 px-2 mb-2 bg-inherit border-2 border-orange-400 rounded-2xl hover:bg-orange-200 ">
+			<div className="flex justify-between items-center mt-5 mx-5">
+				<label className="text-orange-400 text-lg font-bold pb-2">Top Sellers</label>
+				{/* <Button className=" p-0 px-2 mb-2 bg-inherit border-2 border-orange-400 rounded-2xl hover:bg-orange-200 ">
 					Show More
-				</Button>
+				</Button> */}
 			</div>
 			<Progress
 				size="sm"
 				color="warning"
 				value={100}
-				className="font-bold text-orange-400 pb-5"
+				className="font-bold text-orange-400 pb-5 px-4"
 			/>
-			<div className="w-full grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-1 gap-6 mb-2">
+			<div className="w-full grid md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-1 gap-6 mb-2 lg:px-5">
 				{productSections.map((product) => (
 					<CategoriesSlider
 						key={product.id}
