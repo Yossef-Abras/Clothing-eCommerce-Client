58f66d6bc03a182f823f5591d2a53c1c063f337e
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-08-29 19:35:06 +0300

    fixing name method

--- public/global/wishlist.js
+++ public/global/wishlist.js
@@ -1,50 +1,50 @@
 import axios from "axios";
 export const getWishlist = async () => {
-    let token = localStorage.getItem("token");
-    if (token) {
-        try {
-            const response = await axios.get(`${process.env.BASE_API_URL}/wishlist`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            return response.data.data
-        } catch (error) {
-            throw error.response.data;
-        }
+  let token = localStorage.getItem("token");
+  if (token) {
+    try {
+      const response = await axios.get(`${process.env.BASE_API_URL}/wishlist`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return response.data.data;
+    } catch (error) {
+      throw error.response.data;
     }
-
+  }
 };
-export const createWislist = async (productId) => {
-    let token = localStorage.getItem("token");
-    if (token) {
-        try {
-            const response = await axios.post(`${process.env.BASE_API_URL}/wishlist`, { productId },
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                });
-            return response.data
-        } catch (error) {
-            throw error.response.data;
+export const addWishlist = async (productId) => {
+  let token = localStorage.getItem("token");
+  if (token) {
+    try {
+      const response = await axios.post(
+        `${process.env.BASE_API_URL}/wishlist`,
+        { productId },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
+      );
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
     }
+  }
 };
 export const deletProductFromWishlist = async (productId) => {
-    let token = localStorage.getItem("token");
-    if (token) {
-        try {
-            await axios.delete(`${process.env.BASE_API_URL}/wishlist/${productId}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            return true;
-        } catch (error) {
-            throw error.response.data;
-        }
+  let token = localStorage.getItem("token");
+  if (token) {
+    try {
+      await axios.delete(`${process.env.BASE_API_URL}/wishlist/${productId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      return true;
+    } catch (error) {
+      throw error.response.data;
     }
-
+  }
 };
-
