8626e8ab52f05c19cc5b264ad8c76d75c926e37c
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-09-01 19:21:33 +0300

    program payment methods

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,18 +1,37 @@
-import Cart from "../components/Cart";
 import { useEffect, useState } from "react";
-import { Button, Spinner } from "@nextui-org/react";
+import { Button, Spinner, Input, Select, SelectItem } from "@nextui-org/react";
 import { getCart } from "../../public/global/cart";
+import {
+  createCheckoutSession,
+  createCashOrder,
+} from "../../public/global/order";
+import { useRouter } from "next/router";
+import Cart from "../components/Cart";
+import { TbLogs } from "react-icons/tb";
 
 export default function CartSellers() {
+  const [cartId, setCartId] = useState(null);
   const [cart, setCart] = useState([]);
   const [totalCartPrice, setTotalCartPrice] = useState(0);
   const [loading, setLoading] = useState(true);
+  const [checkoutLoading, setCheckoutLoading] = useState(false);
+  const [shippingAddress, setShippingAddress] = useState({
+    details: "",
+    phone: "",
+    city: "",
+    postalCode: "",
+  });
+  const [showShippingForm, setShowShippingForm] = useState(false);
+  const [orderSuccess, setOrderSuccess] = useState(false);
+  const [paymentMethod, setPaymentMethod] = useState("card");
+  const router = useRouter();
 
   useEffect(() => {
     setLoading(true);
     const fetchCart = async () => {
       try {
         const cart = await getCart();
+        setCartId(cart.data._id);
         setCart(cart.data.cartItems);
         setTotalCartPrice(cart.data.totalCartPrice);
       } catch (error) {
@@ -29,6 +48,43 @@ export default function CartSellers() {
     setTotalCartPrice(totalCartPrice);
   };
 
+  const handleCheckout = async () => {
+    setCheckoutLoading(true);
+    try {
+      if (paymentMethod === "cash") {
+        // Handle cash order creation
+        const cashOrderResponse = await createCashOrder(
+          cartId,
+          shippingAddress
+        );
+        if (cashOrderResponse.status === "success") {
+          setOrderSuccess(true);
+          router.push("/orders");
+        }
+      } else {
+        // Handle card payment
+        const sessionResponse = await createCheckoutSession(
+          cartId,
+          shippingAddress
+        );
+        if (sessionResponse.status === "success") {
+          window.location.href = sessionResponse.url; // Redirect to Stripe Checkout
+        } else {
+          console.error("Failed to create checkout session");
+        }
+      }
+    } catch (error) {
+      console.error("Error during checkout", error);
+    } finally {
+      setCheckoutLoading(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setShippingAddress((prev) => ({ ...prev, [name]: value }));
+  };
+
   if (loading) {
     return (
       <div className="min-h-screen flex items-center justify-center text-orange-400">
@@ -38,8 +94,17 @@ export default function CartSellers() {
   }
 
   return (
-    <div className="w-full min-h-screen p-5 bg-gray-100">
+    <div className="w-full min-h-screen p-5 bg-gray-100 relative">
       <h1 className="text-3xl font-bold mb-8 text-orange-400">Shopping Cart</h1>
+      <Button
+        auto
+        flat
+        className="absolute top-5 right-4"
+        onClick={() => router.push("/orders")}
+      >
+        <TbLogs />
+        Orders
+      </Button>
 
       {cart.length > 0 ? (
         <div className="space-y-6">
@@ -62,11 +127,88 @@ export default function CartSellers() {
               <span className="text-xl font-semibold">Total:</span>
               <span className="text-xl font-semibold">$ {totalCartPrice}</span>
             </div>
-            <div className="text-center">
-              <Button className="bg-orange-400 text-white px-6 py-2 rounded-lg">
-                Proceed to Checkout
-              </Button>
-            </div>
+            {!showShippingForm ? (
+              <div className="text-center">
+                <Button
+                  className="bg-orange-400 text-white px-6 py-2 rounded-lg"
+                  onClick={() => setShowShippingForm(true)}
+                >
+                  Proceed to Checkout
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <div className="mb-4">
+                  <Select
+                    defaultSelectedKeys={["card"]}
+                    disallowEmptySelection={true}
+                    label="Payment Method"
+                    placeholder="Select a payment method"
+                    variant="bordered"
+                    // selectedKeys={[paymentMethod]}
+                    onChange={(e) => setPaymentMethod(e.target.value)}
+                    className="max-w-xs"
+                  >
+                    <SelectItem key="cash" value="cash">
+                      Cash
+                    </SelectItem>
+                    <SelectItem key="card" value="card">
+                      Card
+                    </SelectItem>
+                  </Select>
+                </div>
+                <div className="mb-4">
+                  <Input
+                    name="details"
+                    label="Shipping Details"
+                    value={shippingAddress.details}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <Input
+                    name="phone"
+                    label="Phone Number"
+                    value={shippingAddress.phone}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <Input
+                    name="city"
+                    label="City"
+                    value={shippingAddress.city}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="mb-4">
+                  <Input
+                    name="postalCode"
+                    label="Postal Code"
+                    value={shippingAddress.postalCode}
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <div className="text-center">
+                  {orderSuccess && (
+                    <p className="text-green-600 mb-4">
+                      Redirecting to payment...
+                    </p>
+                  )}
+                  <Button
+                    className="bg-orange-400 text-white px-6 py-2 rounded-lg"
+                    onClick={handleCheckout}
+                    isDisabled={checkoutLoading}
+                  >
+                    {checkoutLoading ? (
+                      <Spinner color="white" size="sm" />
+                    ) : (
+                      "Proceed to Payment"
+                    )}
+                  </Button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       ) : (
