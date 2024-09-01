b7cc90ca6cb079112f73d04df04dc9d4583fbeb3
Mouhammed Dbs <mouhammeddbs@gmail.com>
2024-09-01 19:40:36 +0300

    fix responsive

--- src/pages/orders/index.js
+++ src/pages/orders/index.js
@@ -33,22 +33,23 @@ export default function Orders() {
 
   return (
     <div className="w-full min-h-screen p-5 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-8 text-orange-400">Your Orders</h1>
+      <h1 className="text-3xl font-bold mb-1 text-orange-400">Your Orders</h1>
+      <p className="text-xs text-gray-400 pl-1">
+        Click on order item to show details
+      </p>
       {orders.length > 0 ? (
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 mt-8">
           {orders.map((order) => (
             <Link key={order._id} href={`/orders/${order._id}`} passHref>
-              <div className="flex items-center justify-between p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow border border-gray-200 cursor-pointer">
+              <div className="flex flex-col md:flex-row md:items-center justify-between p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow border border-gray-200 cursor-pointer">
                 <div className="flex items-center">
                   <FaCalendarDay className="text-orange-500 mr-2" />
-                  <span className="font-semibold text-gray-700">Order ID:</span>
+                  <span className="font-semibold text-gray-700">ID:</span>
                   <span className="ml-2 text-gray-600">{order._id}</span>
                 </div>
                 <div className="flex items-center">
                   <FaDollarSign className="text-orange-500 mr-2" />
-                  <span className="font-semibold text-gray-700">
-                    Total Price:
-                  </span>
+                  <span className="font-semibold text-gray-700">Price:</span>
                   <span className="ml-2 text-gray-600">
                     ${order.totalOrderPrice}
                   </span>
