090ec8ba8b687821908285d266c903aa8e1368d4
yossef_abras <yossefabras@gmail.com>
2024-10-15 13:27:43 +0300

    fixing

--- public/global/helper.js
+++ public/global/helper.js
@@ -1,19 +1,9 @@
-export const getToken = () => {
-    return localStorage.getItem("token");
-};
-
-export const setToken = (token) => {
-    localStorage.setItem("token", token);
-};
-
-export const removeToken = () => {
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-};
+import store from "../../src/store/store";
+import { logout } from "../../src/store/userSlice";
 
 export const handle401Error = (error) => {
     if (error.response && error.response.status === 401) {
-        removeToken();
+        store.dispatch(logout());
     }
     throw error;
 };
