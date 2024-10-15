5b4a4971f270999381b16ec735f57b0fb11e6e08
yossef_abras <yossefabras@gmail.com>
2024-10-15 13:35:38 +0300

    saving state redux in localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,36 @@
-// store/index.js
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+
+const saveStateToLocalStorage = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem('reduxState', serializedState);
+    } catch (e) {
+        console.warn("Could not save state", e);
+    }
+};
+const loadStateFromLocalStorage = () => {
+    try {
+        const serializedState = localStorage.getItem('reduxState');
+        if (serializedState === null) return undefined; // إذا لم توجد حالة مخزنة
+        return JSON.parse(serializedState);
+    } catch (e) {
+        console.warn("Could not load state", e);
+        return undefined;
+    }
+};
+
+const preloadedState = loadStateFromLocalStorage();
 
 const store = configureStore({
     reducer: {
         user: userReducer,
     },
+    preloadedState,
+});
+
+store.subscribe(() => {
+    saveStateToLocalStorage(store.getState());
 });
 
 export default store;
